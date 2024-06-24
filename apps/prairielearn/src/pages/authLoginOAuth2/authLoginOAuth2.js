f3883c6b940c1cd5e9b857ff8172084d57ef063e
Gregory Bell <gjbell@mines.edu>
2024-06-23 22:00:43 -0600

    Add comments for oauth flow migration to microsoft from google

--- apps/prairielearn/src/pages/authLoginOAuth2/authLoginOAuth2.js
+++ apps/prairielearn/src/pages/authLoginOAuth2/authLoginOAuth2.js
@@ -1,6 +1,8 @@
 // @ts-check
 import { Router } from 'express';
 import asyncHandler from 'express-async-handler';
+// Need to change to MSFT oauth library
+// I think this one?? https://github.com/AzureAD/microsoft-authentication-library-for-js
 import { OAuth2Client } from 'google-auth-library';
 
 import { HttpStatusError } from '@prairielearn/error';
@@ -9,6 +11,10 @@ import { config } from '../../lib/config.js';
 
 const router = Router();
 
+/**
+  * Need to update to use EntraID stuff
+  * Should be pretty similar - it seems to just be yoiking from the config file
+  * */
 router.get(
   '/',
   asyncHandler(async (req, res) => {
@@ -28,6 +34,7 @@ router.get(
     );
     const url = oauth2Client.generateAuthUrl({
       access_type: 'online',
+      // are these scopes enough??
       scope: ['openid', 'profile', 'email'],
       prompt: 'select_account',
       // FIXME: should add some state here to avoid CSRF
