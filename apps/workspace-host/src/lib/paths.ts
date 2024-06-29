9ba0ff7d134e75c0e84c593656c8f5826ed46f78
Gregory Bell <gjbell@mines.edu>
2024-06-29 14:09:44 -0600

    Enable azure auth

--- apps/workspace-host/src/lib/paths.ts
+++ apps/workspace-host/src/lib/paths.ts
@@ -3,3 +3,9 @@ import * as path from 'node:path';
 export const REPOSITORY_ROOT_PATH = path.resolve(import.meta.dirname, '..', '..', '..', '..');
 
 export const APP_ROOT_PATH = path.resolve(import.meta.dirname, '..', '..');
+
+
+// I want to be able to define an ENV var where we should look for the config file
+// so that we can support docker secrets without having to use docker plugins
+
+export const CONFIG_FILE_PATH = process.env.CONFIG_FILE_PATH;
--- docker/gen_ssl.sh
+++ docker/gen_ssl.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-KEY=/etc/pki/tls/private/localhost.key
-CERT=/etc/pki/tls/certs/localhost.crt
+KEY=/etc/pki/tls/private/backus.mines.edu.key
+CERT=/etc/pki/tls/certs/backus.mines.edu.crt
 CA_CHAIN=/etc/pki/tls/certs/server-chain.crt
 
 if [ -e $KEY ]; then
@@ -10,8 +10,8 @@ fi
 
 openssl req -x509 -out $CERT -keyout $KEY \
     -newkey rsa:2048 -nodes -sha256 \
-    -subj '/CN=localhost' -extensions EXT -config <( \
-    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+    -subj '/CN=backus.mines.edu' -extensions EXT -config <( \
+    printf "[dn]\nCN=backus.mines.edu\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:backus.mines.edu\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
 # Make sure the file exists but can be empty
 touch $CA_CHAIN
